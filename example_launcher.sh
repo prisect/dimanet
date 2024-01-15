a5196f4c7442a3783a805992346ff2ee61922e64
dimaaac <78304878+dimaaac@users.noreply.github.com>
2024-01-15 08:22:43 +0100

    Update example_launcher.sh

--- example_launcher.sh
+++ example_launcher.sh
@@ -21,14 +21,15 @@ while true; do
     read -p "1-5: -$ " choice
 
     case $choice in
-        1) ./examples/example_1.o && break ;;
-        2) ./examples/example_2.o && break ;;
-        3) ./examples/example_3.o && break ;;
-        4) ./examples/example_4.o && break ;;
-        5) ./examples/example_5.o && break ;;
+        1) ./examples/example1 && break ;;
+        2) ./examples/example2 && break ;;
+        3) ./examples/example3 && break ;;
+        4) ./examples/example4 && break ;;
+        5) ./examples/example5 && break ;;
         *)
             echo "Invalid choice. Enter a number between 1 and 5."
             continue
             ;;
     esac
 done
+
