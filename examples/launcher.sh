ff707e57330e2ba2554c946e75af7a1264d453ac
dimaaac <78304878+dimaaac@users.noreply.github.com>
2024-01-15 21:12:28 +0100

    Create launcher.sh

---
+++ examples/launcher.sh
@@ -0,0 +1,35 @@
+# this script is made to launch default
+# example scripts from the source code
+#
+# i wont call this a framework tho
+# lmfao
+
+#!/bin/bash
+
+echo "DimaNet Example Launcher"
+echo ""
+echo "Select a C example to run:"
+echo " 1 - ex1::XOR Function using Backpropagation training"
+echo " 2 - ex2::XOR Function using Random Search training"
+echo " 3 - ex3::Loads and runs an ANN from a file"
+echo " 4 - ex4::IRIS data-set using Backpropagation training"
+echo " 5 - ex5::Visualization of ANN Approximation"
+echo ""
+echo "WARNING: RUN MAKE BEFORE RUNNING THE SCRIPT!"
+
+while true; do
+    read -p "1-5: -$ " choice
+
+    case $choice in
+        1) ./examples/example1 && continue ;;
+        2) ./examples/example2 && continue ;;
+        3) ./examples/example3 && continue ;;
+        4) ./examples/example4 && continue ;;
+        5) ./examples/example5 && continue ;;
+        exit) break ;;
+        *)
+            echo "Invalid choice. Enter a number between 1 and 5."
+            continue
+            ;;
+    esac
+done
