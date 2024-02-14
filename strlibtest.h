ea18e0e5f2dbfe4ec136878ee112dfaf35365974
dima <78304878+dimalmfao@users.noreply.github.com>
2024-02-14 07:50:14 +0100

    Create strlibtest.h

---
+++ strlibtest.h
@@ -0,0 +1,12 @@
+#ifndef STRLIBTEST_H
+#define STRLIBTEST_H
+
+#include "strlib.h"
+void t_str_cat();
+void t_str_substring();
+void t_str_split();
+void t_str_length();
+void t_strchr();
+void t_strrchr();
+void t_str_compare();
+#endif
